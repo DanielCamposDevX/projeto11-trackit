61ea8e88937e732d688c8c8c4a66192a64a15aaf
DanielCamposDevX <daniel.campos.dev3@gmail.com>
2023-05-31 22:00:01 -0300

    Login e cadastro prontos

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,9 +1,8 @@
-import { createContext, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import logo from '../assets/Group 8.png'
 import axios from "axios";
-import { useContext } from "react";
 
 
 
